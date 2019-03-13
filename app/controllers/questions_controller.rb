c968d3f7d2fed0cb1207d8affafdc8797fbe5f7f
Naveen Honest Raj <naveen@skcript.com>
2019-03-13 22:09:53 +0530

    adding controllers with assumptional variables
    Signed-off-by: Naveen Honest Raj <naveen@skcript.com>

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+
+  def index
+    question_set = QuestionSet.new
+    render json: question_set.questions
+  end
+
+  def choose
+    question_set = QuestionSet.new
+    questions = question_set.choose(100, {easy: 20, medium: 40, hard: 40})
+    render json: questions
+  end
+end
